bd89f366cfb1c19c99c3341a30a9c33e62098cb7
this-is-matt <mcp6000@gmail.com>
2022-04-30 16:14:21 -0600

    uploading ugly db code, but now password

--- server.js
+++ server.js
@@ -2,8 +2,25 @@ const express = require('express')
 const app = express()
 const port = process.env.PORT || 3000
 
+
 app.get('/', require('./routes'))
 
 app.listen(port, () => {
     console.log(`Example app listening on port ${port}`)
 })
+
+
+//clean code
+const dotenv = require('dotenv');
+dotenv.config();
+
+const MongoClient = require('mongodb').MongoClient;
+MongoClient.connect(process.env.MONGODB_URI, function(err, db){
+    if (err) throw err;
+    var dbo = db.db("contacts");
+    dbo.collection("contacts").find().toArray(function(err, result){
+        if(err) throw err;
+        console.log(result);
+        db.close();
+    });
+});
