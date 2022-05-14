6635e24caf23ac99b9d95c6ec1a6d5c3db686c6b
this-is-matt <mcp6000@gmail.com>
2022-05-14 13:34:11 -0600

    bypassed dotenv to see if heroku will work now.

--- db/connect.js
+++ db/connect.js
@@ -1,6 +1,6 @@
 //.env variables MONGODB_URI
-const dotenv = 'dotenv';
-dotenv.config();
+// const dotenv = require('dotenv');
+// dotenv.config();
 
 
 // database code
@@ -9,7 +9,8 @@ const MongoClient = require('mongodb').MongoClient;
 let _client;
 let _collection;
 const initDatabase = () => {
-    MongoClient.connect(process.env.MONGODB_URI, (err, client) => {
+    // MongoClient.connect(process.env.MONGODB_URI, (err, client) => {
+    MongoClient.connect('mongodb+srv://app:node1234@cluster0.hrjjy.mongodb.net/contacts?retryWrites=true&w=majority', (err, client) => {
         if (err) throw err;
         _client = client;
         _collection = client.db("contacts").collection("contacts");
