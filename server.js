2457189dbf1bf49be3923075a0ad1f33982a3fc7
this-is-matt <mcp6000@gmail.com>
2022-05-14 22:27:22 -0600

    Swagger seems to be working, the routes from before still don't work, but swagger is working.

--- server.js
+++ server.js
@@ -4,12 +4,19 @@ const port = process.env.PORT || 3000
 const connect = require('./db/connect');
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
+const cors = require('cors');
+
+//set up swagger
+const swaggerUi = require('swagger-ui-express');
+const swaggerDocument = require('./swagger.json');
 
 connect.initDatabase();
 mongoose.connect('mongodb+srv://app:node1234@cluster0.hrjjy.mongodb.net/contacts?retryWrites=true&w=majority');
 
-app.use(bodyParser.json());
-app.use('/', require('./routes'))
+app.use(bodyParser.json())
+    .use('/', require('./routes'))
+    .use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument))
+    .use(cors())
 
 app.listen(port, () => {
     console.log(`App listening on port ${port}`)
