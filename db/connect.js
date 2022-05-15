af4f8314d81dc28e13137850397573be6cfbdc86
this-is-matt <mcp6000@gmail.com>
2022-05-14 23:16:14 -0600

    testing dotenv again... and also heroku isn't working with api-docs

--- db/connect.js
+++ db/connect.js
@@ -1,7 +1,7 @@
 //.env variables MONGODB_URI
 //spent two hours trying to get this to work. it works in local but not in Heroku. It says missing modules. can't find dotenv. looked online for solutions and read Heroku's documentation on the topic as well. couldn't find the answer.will approach again down the road.
-// const dotenv = require('dotenv');
-// dotenv.config();
+const dotenv = require('dotenv');
+dotenv.config();
 
 
 // database code
@@ -10,8 +10,8 @@ const MongoClient = require('mongodb').MongoClient;
 let _client;
 let _collection;
 const initDatabase = () => {
-    // MongoClient.connect(process.env.MONGODB_URI, (err, client) => {
-    MongoClient.connect('mongodb+srv://app:node1234@cluster0.hrjjy.mongodb.net/contacts?retryWrites=true&w=majority', (err, client) => {
+    MongoClient.connect(process.env.MONGODB_URI, (err, client) => {
+    // MongoClient.connect('mongodb+srv://app:node1234@cluster0.hrjjy.mongodb.net/contacts?retryWrites=true&w=majority', (err, client) => {
         if (err) throw err;
         _client = client;
         _collection = client.db("contacts").collection("contacts");
